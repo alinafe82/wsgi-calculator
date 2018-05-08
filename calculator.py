b8111121b198b8453ab1233d951254aff335819a
Alinafe Matenda <alinafe.matenda@gmail.com>
2018-05-07 18:57:45 -0700

    initial commit

--- calculator.py
+++ calculator.py
@@ -19,11 +19,11 @@ body in my browser should be `15`.
 Consider the following URL/Response body pairs as tests:
 
 ```
-  http://localhost:8080/multiply/3/5   => 15
-  http://localhost:8080/add/23/42      => 65
-  http://localhost:8080/subtract/23/42 => -19
-  http://localhost:8080/divide/22/11   => 2
-  http://localhost:8080/               => <html>Here's how to use this page...</html>
+  http://localhost:8089/multiply/3/5   => 15
+  http://localhost:8089/add/23/42      => 65
+  http://localhost:8089/subtract/23/42 => -19
+  http://localhost:8089/divide/22/11   => 2
+  http://localhost:8089/               => <html>Here's how to use this page...</html>
 ```
 
 To submit your homework:
@@ -40,45 +40,113 @@ To submit your homework:
 
 
 """
+def divide(x,y):
+    try:
+        #suppose that number2 is a float
+        return x/y
+    except ZeroDivisionError:
+        return "Cannot divide by 0"
+
+calc_options = {'add': lambda x, y: x + y,
+            'subtract': lambda x, y: x - y,
+            'multiply': lambda x, y: x * y,
+            'divide': lambda x, y:divide(x,y),
+           }
 
 
-def add(*args):
-    """ Returns a STRING with the sum of the arguments """
+def landing():
+    """ TODO: copy description above or add some description """
 
-    # TODO: Fill sum with the correct value, based on the
-    # args provided.
-    sum = "0"
+    landing_page = """<html>
+    <head>
+    <title>WSGI Calculator</title>
+    </head>
+    <body>
+    The WSGI calculator supports the following operations:<br>
+    Addition<br>
+    Subtraction<br>
+    Multiplication<br>
+    Division<br>
+    <br>
+    Examples:<br>
+    http://localhost:8089/multiply/3/5  => 15<br>
+    http://localhost:8089/add/23/42     => 65<br>
+    http://localhost:8089/divide/6/0    => HTTP "400 Bad Request"<br>
+    </body>
+    </html>"""
 
-    return sum
+    return landing_page
 
-# TODO: Add functions for handling more arithmetic operations.
 
+def math_proc(operator, operands):
+    """ Perform math operation specified by 'operator' on 'operands' """
+    
+    result_page = """<html>
+    <head>
+    <title>WSGI Calculator</title>
+    </head>
+    <body>
+    Answer: {}
+    </body>
+    </html>"""
+
+    try:
+        results = calc_optinos[operator.lower()](operands[0], operands[1])
+    except:
+        raise ValueError
+
+    return result_page.format(answer)
+
+    
 def resolve_path(path):
-    """
-    Should return two values: a callable and an iterable of
-    arguments.
-    """
+    """ Resolve the path and return body """
 
-    # TODO: Provide correct values for func and args. The
-    # examples provide the correct *syntax*, but you should
-    # determine the actual values of func and args using the
-    # path.
-    func = add
-    args = ['25', '32']
+    parts = path.strip('/').split('/')
+    if not parts[0]:
+        return landing()
 
-    return func, args
+    elif len(parts) == 3:
+        return math_proc(parts[0], [int(i) for i in parts[1:]])
+
+    else:
+        raise NameError
 
 def application(environ, start_response):
-    # TODO: Your application code from the book database
-    # work here as well! Remember that your application must
-    # invoke start_response(status, headers) and also return
-    # the body of the response in BYTE encoding.
-    #
-    # TODO (bonus): Add error handling for a user attempting
-    # to divide by zero.
-    pass
+    """ A WSGI application for doing basic math via the url """
+
+    import pprint
+    pprint.pprint(environ)
+
+    headers = [("Content-type", "text/html")]
+
+    try:
+        path = environ.get('PATH_INFO', None)
+        body = resolve_path(path)
+        status = "200 OK"
+
+    except NameError:
+        status = "404 Not Found"
+        body = "<h1>Not Found</h1>"
+        body += landing()
+
+    except ValueError:
+        status = "400 Bad Request"
+        body = "<h1>Bad Request</h1>"
+        body += landing()
+
+    except Exception:
+        status = "500 Internal Server Error"
+        body = "<h1>Internal Server Error</h1>"
+        body += landing()
+
+    finally:
+        headers.append(('Content-length', str(len(body))))
+        start_response(status, headers)
+
+        return [body]
+
 
 if __name__ == '__main__':
-    # TODO: Insert the same boilerplate wsgiref simple
-    # server creation that you used in the book database.
-    pass
+    from wsgiref.simple_server import make_server
+    srv = make_server('localhost', 8080, application)
+    srv.serve_forever()
