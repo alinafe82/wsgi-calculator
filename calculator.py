55164d2988f856c3c00bb41b878f7cf56a0f8665
Alinafe Matenda <alinafe.matenda@gmail.com>
2018-05-07 21:23:06 -0700

    draft commit

--- calculator.py
+++ calculator.py
@@ -1,33 +1,27 @@
+#!/usr/bin/env python
+
 """
 For your homework this week, you'll be creating a wsgi application of
 your own.
-
 You'll create an online calculator that can perform several operations.
-
 You'll need to support:
-
   * Addition
   * Subtractions
   * Multiplication
   * Division
-
 Your users should be able to send appropriate requests and get back
 proper responses. For example, if I open a browser to your wsgi
 application at `http://localhost:8080/multiple/3/5' then the response
 body in my browser should be `15`.
-
 Consider the following URL/Response body pairs as tests:
-
 ```
-  http://localhost:8089/multiply/3/5   => 15
-  http://localhost:8089/add/23/42      => 65
-  http://localhost:8089/subtract/23/42 => -19
-  http://localhost:8089/divide/22/11   => 2
-  http://localhost:8089/               => <html>Here's how to use this page...</html>
+  http://localhost:8090/multiply/3/5   => 15
+  http://localhost:8090/add/23/42      => 65
+  http://localhost:8090/subtract/23/42 => -19
+  http://localhost:8090/divide/22/11   => 2
+  http://localhost:8090/               => <html>Here's how to use this page...</html>
 ```
-
 To submit your homework:
-
   * Fork this repository (Session03).
   * Edit this file to meet the homework requirements.
   * Your script should be runnable using `$ python calculator.py`
@@ -38,115 +32,116 @@ To submit your homework:
   * Commit and push your changes to your fork.
   * Submit a link to your Session03 fork repository!
 
-
 """
-def divide(x,y):
-    try:
-        #suppose that number2 is a float
-        return x/y
-    except ZeroDivisionError:
-        return "Cannot divide by 0"
 
-calc_options = {'add': lambda x, y: x + y,
-            'subtract': lambda x, y: x - y,
-            'multiply': lambda x, y: x * y,
-            'divide': lambda x, y:divide(x,y),
-           }
+html_text = """<html>
+<head>
+<title>WSGI Calulator </title>
+</head>
+<body>
+<h1>{print_operand_a} {print_operation_sign} {print_operand_b}
+= {print_result}</h1>
+<hr>
+<h3>Some Examples</h3>
+<p><a href="http://localhost:8090/multiply/3/5">multiply/3/5</a></p>
+<p><a href="http://localhost:8090/add/23/42">add/23/42</a></p>
+<p><a href="http://localhost:8090/divide/6/0">divide/6/0</a></p>
+<p><a href="http://localhost:8090/subtract/126/21">subtract/126/21</a></p>
+<p><a href="http://localhost:8090/divide/4223/17">divide/4223/17</a></p>
+<p><a href="http://localhost:8090/subtract/123/0">subtract/123/0</a></p>
+<hr>
+<p>Path Info: {print_path_info} Entries: {print_no_entries}</p>
+<p>Operation: {print_operation} First Operand: {print_operand_a}
+Second Operand: {print_operand_b}</p>
+</body>
+</html>"""
 
-
-def landing():
-    """ TODO: copy description above or add some description """
-
-    landing_page = """<html>
-    <head>
-    <title>WSGI Calculator</title>
-    </head>
-    <body>
-    The WSGI calculator supports the following operations:<br>
-    Addition<br>
-    Subtraction<br>
-    Multiplication<br>
-    Division<br>
-    <br>
-    Examples:<br>
-    http://localhost:8089/multiply/3/5  => 15<br>
-    http://localhost:8089/add/23/42     => 65<br>
-    http://localhost:8089/divide/6/0    => HTTP "400 Bad Request"<br>
-    </body>
-    </html>"""
-
-    return landing_page
-
-
-def math_proc(operator, operands):
-    """ Perform math operation specified by 'operator' on 'operands' """
-    
-    result_page = """<html>
-    <head>
-    <title>WSGI Calculator</title>
-    </head>
-    <body>
-    Answer: {}
-    </body>
-    </html>"""
-
-    try:
-        results = calc_optinos[operator.lower()](operands[0], operands[1])
-    except:
-        raise ValueError
-
-    return result_page.format(answer)
-
-    
-def resolve_path(path):
-    """ Resolve the path and return body """
-
-    parts = path.strip('/').split('/')
-    if not parts[0]:
-        return landing()
-
-    elif len(parts) == 3:
-        return math_proc(parts[0], [int(i) for i in parts[1:]])
-
-    else:
-        raise NameError
 
 def application(environ, start_response):
-    """ A WSGI application for doing basic math via the url """
-
-    import pprint
-    pprint.pprint(environ)
-
     headers = [("Content-type", "text/html")]
-
     try:
         path = environ.get('PATH_INFO', None)
-        body = resolve_path(path)
+        if path is None:
+            raise NameError
+
+        args = resolve_path(path)
+
+        if len(args) != 3:
+            operation = "failed"
+            operation_sign = "f"
+        else:
+            pass
+
+        operation_list = ["multiply", "divide", "add", "subtract"]
+
+        if args[0].strip() in operation_list:
+            operation = args[0].strip()
+        else:
+            operation = "failed"
+            operation_sign = "f"
+
+        try:
+            operand_a = int(args[1])
+        except:
+            operand_a = "error"
+
+        try:
+            operand_b = int(args[2])
+        except:
+            operand_b = "error"
+
+        if operation == "multiply":
+            result = operand_a * operand_b
+            operation_sign = "*"
+        elif operation == "divide":
+            if operand_b == 0:
+                result = "bad request - can't divide by zero"
+                operation_sign = "/"
+            else:
+                result = operand_a / operand_b
+                operation_sign = "/"
+        elif operation == "add":
+            result = operand_a + operand_b
+            operation_sign = "+"
+        elif operation == "subtract":
+            result = operand_a - operand_b
+            operation_sign = "-"
+        elif operation == "failed":
+            result = "error - please try again"
+            operation_sign = "f"
+        else:
+            result = "failer"
+
+        body = html_text.format(
+            print_path_info=path,
+            print_no_entries=len(args),
+            print_operation=operation,
+            print_operation_sign=operation_sign,
+            print_operand_a=operand_a,
+            print_operand_b=operand_b,
+            print_result=result
+        )
         status = "200 OK"
 
     except NameError:
         status = "404 Not Found"
         body = "<h1>Not Found</h1>"
-        body += landing()
-
-    except ValueError:
-        status = "400 Bad Request"
-        body = "<h1>Bad Request</h1>"
-        body += landing()
 
     except Exception:
         status = "500 Internal Server Error"
         body = "<h1>Internal Server Error</h1>"
-        body += landing()
-
     finally:
         headers.append(('Content-length', str(len(body))))
         start_response(status, headers)
+        return [body.encode('utf8')]
 
-        return [body]
+
+def resolve_path(path):
+    args = path.strip("/").split("/")
+    return args
 
 
 if __name__ == '__main__':
     from wsgiref.simple_server import make_server
-    srv = make_server('localhost', 8080, application)
+    srv = make_server('localhost', 8090, application)
     srv.serve_forever()
